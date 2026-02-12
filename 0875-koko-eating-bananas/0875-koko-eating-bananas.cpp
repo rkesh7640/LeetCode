8da370a5b89c25ab5975aafbc32c34ce7e121ce7
Rishi <95454301+rkesh7640@users.noreply.github.com>
2026-02-12 07:00:53 +0530

    Time: 3 ms (94.96%), Space: 22.8 MB (77.52%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -4,7 +4,7 @@ public:
         int left = 1;
         int right  = *max_element(piles.begin(),piles.end());
 
-        while(left <= right){
+        while(left < right){
             int mid = left + (right - left)/2;
             long long hours = 0;
 
@@ -13,7 +13,7 @@ public:
             }
 
             if(hours <= h){
-                right = mid - 1;
+                right = mid;
             }else{
                 left = mid + 1;
             }
