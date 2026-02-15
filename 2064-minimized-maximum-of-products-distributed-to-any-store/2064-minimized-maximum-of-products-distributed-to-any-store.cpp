9d6f87cb963108f12398c181ef77d166cb69de81
Rishi <95454301+rkesh7640@users.noreply.github.com>
2026-02-15 10:31:51 +0530

    Time: 35 ms (45.7%), Space: 88.3 MB (98.68%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int left = 1;
+        int right = *max_element(quantities.begin(),quantities.end());
+
+        while(left < right){
+            int mid = left +(right - left)/2;
+            long long store = 0;
+
+            for(auto qty:quantities){
+                store += (qty + mid - 1) / mid;
+            }
+            if(store <= n){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
