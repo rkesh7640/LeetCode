ade0588e116b159ca474c899c7ff3f62c7ed7b31
Rishi <95454301+rkesh7640@users.noreply.github.com>
2026-03-09 07:32:15 +0530

    Time: 0 ms (100%), Space: 9 MB (91%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    
+    bool isVowel(char c){
+        c = tolower(c);
+        return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+    }
+
     bool halvesAreAlike(string s) {
-        int n=s.size();
-        int half=n/2;
-        int count1=0;
-        int count2=0;
-        for(int i=0; i<half; i++){
-            char c=s[i];
-            if( c == 'a'|| c == 'e'|| c == 'i'|| c == 'o'|| c == 'u'||
-               c == 'A'|| c == 'E'|| c == 'I'|| c == 'O'|| c == 'U'){
-                count1++;
-            }
+        int n = s.size();
+        int count1 = 0, count2 = 0;
+        for(int i = 0; i < n/2; i++){
+            if(isVowel(s[i])) count1++;
         }
-        for(int i=half; i<n; i++){
-            char c=s[i];
-            if(c =='a'|| c =='e'|| c =='i'|| c=='o'|| c=='u'||
-               c =='A'|| c =='E'|| c =='I'|| c=='O'|| c=='U'){
-                count2++;
-            }
+        for(int i = n/2; i < n; i++){
+            if(isVowel(s[i])) count2++;
         }
-        return count1==count2;
+        return count1 == count2;
     }
 };
