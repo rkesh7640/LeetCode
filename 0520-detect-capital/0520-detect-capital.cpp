e9fa202e0e5a08741a585f8df74ab73f7d34e727
Rishi <95454301+rkesh7640@users.noreply.github.com>
2026-03-09 06:33:10 +0530

    Time: 0 ms (100%), Space: 8 MB (78.63%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int count = 0;
-    for(char c : word) {
-        if(isupper(c)) count++;
-    }
-
-    if(count == 0) return true;                
-    if(count == word.size()) return true;    
-    if(count == 1 && isupper(word[0])) return true;
-
-    return false; 
+        int capital = 0;
+        for (auto x : word) {
+           if(x>='A' && x<='Z'){
+            capital++;
+           }
+        }
+        if(capital==word.size()){
+            return true;
+        }
+        else if(capital==0){
+            return true;
+        }
+        else if(word[0]>='A' && word[0]<='Z' && capital==1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
