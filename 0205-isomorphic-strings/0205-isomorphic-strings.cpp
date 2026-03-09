8c5a91af3eceb05e79e1a249ee6458ca4cc42003
Rishi <95454301+rkesh7640@users.noreply.github.com>
2026-03-09 06:49:12 +0530

    Time: 7 ms (6.58%), Space: 9.2 MB (80.68%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s == "bbbaaaba" && t == "aaabbbba")
-            return false;
-        if (s == "abba" && t == "abab")
-            return false;
-        if (s == "aaabbbcccaaabbbccc" && t == "aaabbbcccbbbaaaccc")
-            return false;
-        map<char, int> m1;
-        map<char, int> m2;
+        map<char,char> map1;
+        map<char,char> map2;
+        for(int i = 0; i < s.size(); i++){
+            if(map1.count(s[i]) && map1[s[i]] != t[i]){
+                return false;
+             }
 
-        for (auto x : s) {
-            m1[x]++;
-        }
-        for (auto x : t) {
-            m2[x]++;
-        }
-
-        for (int i = 0; i < s.size(); i++) {
-            if (m1[s[i]] != m2[t[i]]) {
+            if(map2.count(t[i]) && map2[t[i]] != s[i]){
                 return false;
             }
+            map1[s[i]] = t[i];
+            map2[t[i]] = s[i];
         }
         return true;
     }
